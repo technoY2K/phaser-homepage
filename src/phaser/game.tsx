f86234d9f29d4c82d0ea39584ffae1d9afb86d8e
technoY2K <cloud@kevia.me>
2022-07-25 19:14:07 -0700

    feat: canvas resize is working

--- src/phaser/game.tsx
+++ src/phaser/game.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import Phaser from "phaser";
 
+enum GameState {
+  Ready = "READY",
+}
 class Scene extends Phaser.Scene {
   create() {
     const screenCenterX =
@@ -9,24 +12,58 @@ class Scene extends Phaser.Scene {
       this.cameras.main.worldView.y + this.cameras.main.height / 2;
     this.add.text(screenCenterX, screenCenterY, "Welcome").setOrigin(0.5);
 
-    this.game.events.emit("READY", true);
+    this.game.events.emit(GameState.Ready, true);
   }
 }
 
 export function Game() {
   const [isReady, setReady] = useState(false);
 
+  function windowSizedChanged(game: Phaser.Game) {
+    if (game.isBooted) {
+      setTimeout(() => {
+        game.scale.resize(window.innerWidth, window.innerHeight);
+        game.canvas.setAttribute(
+          "style",
+          `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`
+        );
+      }, 100);
+    }
+  }
+
   useEffect(() => {
     const config: Phaser.Types.Core.GameConfig = {
-      type: Phaser.AUTO,
       parent: "game",
-      width: "100%",
-      height: 600,
+      title: "Cloud Land",
+      type: Phaser.WEBGL,
+      backgroundColor: "#351f1b",
+      scale: {
+        mode: Phaser.Scale.ScaleModes.NONE,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+      physics: {
+        default: "arcade",
+        arcade: {
+          debug: false,
+        },
+      },
+      render: {
+        antialiasGL: false,
+        pixelArt: true,
+      },
+      callbacks: {
+        postBoot: (game) => {
+          window.onresize = () => windowSizedChanged(game);
+        },
+      },
+      canvasStyle: `display: block; width: 100%; height: 100%;`,
+      autoFocus: true,
       scene: [Scene],
     };
 
     const game = new Phaser.Game(config);
-    game.events.on("READY", setReady);
+    game.events.on(GameState.Ready, setReady);
 
     return () => {
       setReady(false);
