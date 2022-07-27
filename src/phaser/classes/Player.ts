a9caafa47d33b09861b52a13cbb3a4c196bd0644
technoY2K <cloud@kevia.me>
2022-07-26 19:52:01 -0700

    feat: finalize running animation

--- src/phaser/classes/Player.ts
+++ src/phaser/classes/Player.ts
@@ -29,22 +29,26 @@ export class Player extends Actor {
 
         if (this.keyW?.isDown) {
             this.body.velocity.y = -110;
+            !this.anims.isPlaying && this.anims.play("run", true);
         }
 
         if (this.keyA?.isDown) {
             this.body.velocity.x = -110;
             this.checkFlip();
             this.getBody().setOffset(48, 15);
+            !this.anims.isPlaying && this.anims.play("run", true);
         }
 
         if (this.keyS?.isDown) {
             this.body.velocity.y = 110;
+            !this.anims.isPlaying && this.anims.play("run", true);
         }
 
         if (this.keyD?.isDown) {
             this.body.velocity.x = 110;
             this.checkFlip();
             this.getBody().setOffset(15, 15);
+            !this.anims.isPlaying && this.anims.play("run", true);
         }
     }
 
