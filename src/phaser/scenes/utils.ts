8c807c73ec4facfa6c6e203190bfb3cab7e74b7e
technoY2K <cloud@kevia.me>
2022-07-29 22:39:55 -0700

    feat: create utility to convert game objects to points

---
+++ src/phaser/scenes/utils.ts
@@ -0,0 +1,18 @@
+type ObjectPoint = {
+    height: number;
+    id: number;
+    name: string;
+    point: boolean;
+    rotation: number;
+    type: string;
+    visible: boolean;
+    width: number;
+    x: number;
+    y: number;
+};
+
+export function gameObjectsToObjectPoints(
+    gameObjects: unknown[]
+): ObjectPoint[] {
+    return gameObjects.map((g) => g as ObjectPoint);
+}
