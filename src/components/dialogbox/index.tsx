33c8e742561652696c43ab79fd94fb0d1a7b2f9b
technoY2K <cloud@kevia.me>
2022-08-03 20:43:15 -0700

    feat: listen to keyboard events

--- src/components/dialogbox/index.tsx
+++ src/components/dialogbox/index.tsx
@@ -1,3 +1,5 @@
+import React, { useCallback, useEffect, useState } from "react";
+
 type DialogBoxProps = {
     messages: string[];
     characterName: string;
@@ -12,4 +14,37 @@ export default function DialogBox({
     onDialogEnded,
     screenWidth,
     screenHeight,
-}: DialogBoxProps) {}
+}: DialogBoxProps) {
+    const [currentMessage, setCurrentMessage] = useState(0);
+    const [messageEnded, setMessageEnded] = useState(false);
+    const [forceShowFullMessage, setForceShowFullMessage] = useState(false);
+
+    useEffect(() => {
+        const handleKeyPressed = (e: KeyboardEvent) => {
+            if (["Enter", "Space", "Escape"].includes(e.code)) {
+                handleClick();
+            }
+        };
+
+        window.addEventListener("keydown", handleKeyPressed);
+
+        return () => window.removeEventListener("keydown", handleKeyPressed);
+    }, []);
+
+    const handleClick = useCallback(() => {
+        if (messageEnded) {
+            setMessageEnded(false);
+            setForceShowFullMessage(false);
+
+            if (currentMessage < messages.length - 1) {
+                setCurrentMessage(currentMessage + 1);
+            } else {
+                setCurrentMessage(0);
+                onDialogEnded();
+            }
+        } else {
+            setMessageEnded(true);
+            setForceShowFullMessage(true);
+        }
+    }, [currentMessage, messageEnded, messages.length, onDialogEnded]);
+}
